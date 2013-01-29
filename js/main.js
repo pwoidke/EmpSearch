ab930b8b2aa18069ba8094018cbde077e13ec703
Paul W. <pwoidke@gmail.com>
2013-01-29 14:16:28 -0500

    Fixed popup, readme, php
    Updated ReadMe. Switched modal dialog implementation to jQuery so that
    everything is a bit more customizable, and added basic PHP to the form
    and PHP folder to make sure everything is submitting correctly.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$('.modalOpen').click(function(){
+    $(".modalDialog")
+        .css({display:'block'})
+        .animate({opacity:"1.0"}, 300);
+});
 
+$('.modalDialog, .modalClose').click(function(){
+    $(".modalDialog")
+        .animate({opacity:"0.0"}, 300, function(){
+            $('.modalDialog').css({display:'none'});
+        })
+});
+
+$('.modalContent').click(function(e){
+    e.stopPropagation();
+});
---
+++ php/send.php
@@ -0,0 +1,12 @@
+<?php
+$name= $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $message;
+echo "<br>";
+?>
