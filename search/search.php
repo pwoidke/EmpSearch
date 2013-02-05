5fa8db164250ef32ba69ab1c3aa6fb33855cf461
Paul W. <pwoidke@gmail.com>
2013-02-05 11:06:32 -0500

    Updated whole site
    Added modal dialogue to home page, and Updated all pages and stylesheets
    to HTML5 Boilerplate.

---
+++ search/search.php
@@ -0,0 +1,473 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <!-- Page Title and CSS -->
+    <title>Finders Records - Your Independent Vinyl Record Store Since 1971 - Search</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="../css/findersrecords-min.css" />
+        <!--[if IE]>
+        <link rel="stylesheet" type="text/css" href="../css/finders-ie-min.css" />
+        <![endif]-->
+        <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+
+	<!-- Gallery Scripts -->
+	<link rel="stylesheet" href="../css/slimbox.css" type="text/css" media="screen" />
+	<script type="text/javascript" src="../js/mootools-1.3.1-core.js"></script>
+	<script type="text/javascript" src="../js/slimbox.js"></script>
+	<!-- End Gallery Scripts -->
+	
+	<!-- Begin Mailchimp styling -->
+	<link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css" />
+	<style type="text/css">
+		/*<![CDATA[*/
+		#mc_embed_signup{/* Fallback for web browsers that doesn't support RGBa */
+			background: rgb(51, 51, 51);
+			/* RGBa with 0.6 opacity */
+			background: rgba(51, 51, 51, 0);
+			clear:left;
+			font:12px Helvetica,Arial,sans-serif;
+			margin-left: 50px;
+		}
+		/*]]>*/
+	</style>
+	<!-- End Mailchimp styling -->	
+	
+</head>
+
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <!-- Begin Page Container -->
+        <div class="container">
+	
+   	<!-- Begin Header -->
+	<header>
+	
+		<!-- Begin header image -->
+		<div class="headerImg"><a href="http://www.findersrecords.com"><img src="../images/head_bg.jpg" width="1024" height="200" /></a></div>
+		<!-- End header image -->
+
+    </header>
+    <!-- End Header -->
+	<!-- Begin Left Column -->
+	<section class="leftCol" style="width:300px">
+	
+	<!-- Begin Quick search form -->
+	<h2 class="search">Quick Search</h2>
+	<form action="search.php" method="post">
+		<input type="hidden" name="pullDownOptionUserSelectedSearchType" value="Loose Match" />
+		<input type="hidden" name="pullDownOptionUserSelectedFormat" value="All" />
+		<input type="hidden" name="pullDownOptionUserSelectedOrder" value="Best Match" />
+		<input type="hidden" name="checkboxShowAvailableOnly" value="No" />
+		<p class="searchLabels" style="text-align:left;">Artist: <input name="usersSearchQueryArtist" type="text" size="40" /><br />
+		Title: <input name="usersSearchQueryTitle" type="text" size="40" /></p>
+		<input class="searchButton" type="submit" value="Search" />
+	</form>
+    <br style="clear:both;"/><br />
+	<a href="index.html" class="advSearch">Advanced Search</a>
+	<!-- End Quick search form -->
+	
+	<br /><br />
+	
+	<!-- Begin Facebook integration -->
+	<iframe src="http://www.facebook.com/plugins/facepile.php?href=https%3A%2F%2Fwww.facebook.com%2Ffindersrecords&action=Like&size=medium&max_rows=1&width=400&colorscheme=dark&appId=98099171222" scrolling="no" frameborder="0" class="fb" allowTransparency="true" style="width:250px;"></iframe>
+	<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><br/>
+    <fb:like href="https://www.facebook.com/findersrecords" layout="button_count" show_faces="false" width="165" font="verdana" colorscheme="dark"></fb:like>
+	<!-- End Facebook integration -->
+	
+    <br /><br /><br />
+						  
+	<!-- Begin Twitter integration -->
+	<a href="https://twitter.com/FindersRecords" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @FindersRecords</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<!-- End Twitter integration -->
+	
+	<br /><br /><br/><br/>
+	
+	<!-- Begin RSD countdown -->
+    <div class="RSDCountdown">
+	<object height="176" width="300">
+		<param name="allowScriptAccess" value="sameDomain" />
+		<param name="allowFullScreen" value="false" />
+		<param name="movie" value="http://recordstoreday.com/templates/Store/pretty_new/swf/countdown.swf" />
+		<param name="quality" value="high" />
+		<param name="bgcolor" value="#333" />
+		<param name="shuffle" value="true" /> 
+		<embed src="http://recordstoreday.com/templates/Store/pretty_new/swf/countdown.swf" width="300" height="176" bgcolor="#FFF" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" AllowScriptAccess="sameDomain"></embed>
+	</object>
+    </div>
+	<!-- End RSD countdown -->
+	
+	<br />
+	
+	<!-- Begin MailChimp Signup Form -->
+	<div id="mc_embed_signup">
+		<form action= "http://findersrecords.us2.list-manage.com/subscribe/post?u=6b9c13df05321c2a2fa5591b6&amp;id=53205c78c0" method="post" id="mc-embedded-subscribe-form" name= "mc-embedded-subscribe-form" class="validate" target= "_blank">
+		<label for="mce-EMAIL"><h2>Subscribe to our E-Newsletter</h2></label> <input type="email" value="" name= "EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required />
+		<div class="clear">
+			<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
+		</div>
+		</form>
+	</div>
+	<!--End MailChimp Signup Form-->
+	
+	</section>
+	<!-- End Left Column -->
+    
+    <!-- Begin Right Column -->
+	<section class="rightCol" style="width: 700px; text-align:center;">
+		
+	<div class="advSearch" align="left">
+          <h1 align="center">Advanced Search</h1>
+          <table align="center">
+          <form action="search.php" method="post">
+          <tr>
+          <td>
+          <b style="font-size:large; margin-right:20px;">Search type:</b>
+          </td>
+          <td>
+          <select name="pullDownOptionUserSelectedSearchType">
+                  <option>Loose Match</option>
+                  <option>Exact Match</option>
+          </select>
+          </td>
+          </tr>
+          <tr>
+          <td>
+          <b style="font-size:large;">Format:</b>
+          </td>
+          <td>
+          <select name="pullDownOptionUserSelectedFormat">
+                  <option>All</option>
+                  <option>CD</option>
+                  <option>DVD</option>
+                  <option>Vinyl</option>
+                  <option>SA-CD</option>
+                  <option>Accessory</option>
+                  <option>Other</option>
+          </select>
+          </td>
+          </tr>
+          <tr>
+          <td>
+          <b style="font-size:large;">Sort by:</b>
+          </td>
+          <td>
+          <select name="pullDownOptionUserSelectedOrder">
+                  <option>Best Match</option>
+                  <option>Artist A-Z</option>
+                  <option>Title A-Z</option>
+                  <option>Release Date (Newest First)</option>
+                  <option>Release Date (Oldest First)</option>
+                  <option>Price (Ascending)</option>
+                  <option>Price (Descending)</option>
+          </select>
+          </td>
+          </tr>
+          <tr>
+          <td colspan="2">
+          <b style="font-size:large; margin-right:10px;">Only show available items?</b>
+          <input type="checkbox" name="checkboxShowAvailableOnly" value="Yes" />
+          </td>
+          </tr>
+          <tr>
+          <td colspan="2">
+          <b style="font-size:large; margin-right:10px;">Artist:</b>
+          <input name="usersSearchQueryArtist" type="text" size="40" />
+          </td>
+          </tr>
+          <tr>
+          <td colspan="2">
+          <b style="font-size:large; margin-right:19px;">Title:</b>
+          <input name="usersSearchQueryTitle" type="text" size="40" />
+          </td>
+          </tr>
+          <tr>
+          <td colspan="2">
+          <input class="searchButton" type="submit" value="Search" style="background-color: #FFF350; padding: 5px 10px 5px 10px; border-radius: 5px; font-size: 20px; float: right;" />
+          </td>
+          </tr>
+          </form>
+          </table>
+          
+          
+          
+          <?php
+$usersQueryArtist = $_POST['usersSearchQueryArtist'];
+$usersQueryTitle = $_POST['usersSearchQueryTitle'];
+$usersSearchType = $_POST['pullDownOptionUserSelectedSearchType'];
+$usersFormat = $_POST['pullDownOptionUserSelectedFormat'];
+$usersOrderBy = $_POST['pullDownOptionUserSelectedOrder'];
+$usersShowAvailableResultsOnly = $_POST['checkboxShowAvailableOnly'];
+
+//Set default values of post vars if needed (used for quick search)
+if(!$usersSearchType){$usersSearchType = "Loose Match";}
+if(!$usersFormat){$usersFormat = "All";}
+if(!$usersOrderBy){$usersOrderBy = "Best Match";}
+if(!$usersShowAvailableResultsOnly){$usersShowAvailableResultsOnly = "No";}
+
+if(($usersQueryArtist != "") && ($usersQueryTitle != ""))
+{
+	echo "<div id=\"search_results_table\" style=\"margin-left:30px;\"><p>Searching for: \"" . $usersQueryTitle . "\" by " . $usersQueryArtist . " using " . $usersSearchType . " in all fields.</p><br/>";
+}
+else if($usersQueryArtist != "")
+{
+	echo "<div id=\"search_results_table\" style=\"margin-left:30px;\"><p>Searching for: \"" . $usersQueryArtist . "\" using " . $usersSearchType . " in Artists.</p><br/>";
+}
+else if($usersQueryTitle != "")
+{
+	echo "<div id=\"search_results_table\" style=\"margin-left:30px;\"><p>Searching for: \"" . $usersQueryTitle . "\" using " . $usersSearchType . " in Titles.</p><br/>";
+}
+
+//Connect to the database server
+$dbServer = "[server]";
+$myUserName = "[username]";
+$myPassword = "[password]";
+@mysql_connect($dbServer, $myUserName, $myPassword)
+        or die ("Can't validate my user name/pwd on the local mysql server!");
+
+//echo "Connected to database<br/><br/>";
+
+//Connect to Catalog database (db16310_catalogdb)
+$myDatabaseName = "[database]" ;
+@mysql_select_db($myDatabaseName)
+        or die ("Can't use the database $myDatabaseName !");
+
+
+//Write base SQL statement (based on loose/exact match selection)
+if(($usersQueryArtist != "") || ($usersQueryTitle != ""))
+{
+	if($usersSearchType == "Loose Match")
+	{
+		if(($usersQueryArtist != "") && ($usersQueryTitle != ""))
+		{
+			$sqlQuery = "SELECT *, MATCH (artist) AGAINST ('" . $usersQueryArtist . "') + MATCH (title) AGAINST ('" . $usersQueryTitle . "') AS relevance FROM items WHERE (MATCH (artist) AGAINST ('" . $usersQueryArtist . "') OR MATCH (title) AGAINST ('" . $usersQueryTitle . "')";
+		}
+		else if($usersQueryArtist != "")
+		{
+			$sqlQuery = "SELECT *, MATCH (artist) AGAINST ('" . $usersQueryArtist . "') AS relevance FROM items WHERE (MATCH (artist) AGAINST ('" . $usersQueryArtist . "')";
+		}
+		else if($usersQueryTitle != "")
+		{
+			$sqlQuery = "SELECT *, MATCH (title) AGAINST ('" . $usersQueryTitle . "') AS relevance FROM items WHERE (MATCH (title) AGAINST ('" . $usersQueryTitle . "')";
+		}
+	}
+	else
+	{
+		if(($usersQueryArtist != "") && ($usersQueryTitle != ""))
+		{
+			$sqlQuery .= "SELECT * FROM items WHERE (artist LIKE '%" . $usersQueryArtist . "%' OR title LIKE '%" . $usersQueryTitle . "%'";
+		}
+		else if($usersQueryArtist != "")
+		{
+			$sqlQuery .= "SELECT * FROM items WHERE (artist LIKE '%" . $usersQueryArtist . "%'";
+		}
+		else if($usersQueryTitle != "")
+		{
+			$sqlQuery .= "SELECT * FROM items WHERE (title LIKE '%" . $usersQueryTitle . "%'";
+		}
+	}
+}
+else
+{
+	$sqlQuery .= "SELECT * FROM items WHERE (ID > 0";
+}
+$sqlQuery .= ")";
+
+//Concatenate Availablility conditional
+if($usersShowAvailableResultsOnly == "Yes")
+{
+	$sqlQuery .= " AND QtyAvail > 0";
+}
+
+//Concatenate Format conditional
+if($usersFormat != "All")
+{
+	if($usersFormat == "CD")
+	{
+		$sqlQuery .= " AND ConfigAbbr = 'CD'";
+	}
+	else if($usersFormat == "DVD")
+	{
+		$sqlQuery .= " AND ConfigAbbr = 'DVD' OR ConfigAbbr = 'DVD-A'";
+	}
+	else if($usersFormat == "Vinyl")
+	{
+		$sqlQuery .= " AND ConfigAbbr = 'LP'";
+	}
+	else if($usersFormat == "DVD")
+	{
+		$sqlQuery .= " AND ConfigAbbr = 'DVD' OR ConfigAbbr = 'DVD-A'";
+	}
+	else if($usersFormat == "SA-CD")
+	{
+		$sqlQuery .= " AND ConfigAbbr = 'SACD'";
+	}
+	else if($usersFormat == "Accessory")
+	{
+		$sqlQuery .= " AND ConfigAbbr = 'ACC'";
+	}
+	else if($usersFormat == "Other")
+	{
+		$sqlQuery .= " AND ConfigAbbr = 'CASS' OR ConfigAbbr = 'VHS'";
+	}
+}
+
+//Block porn results
+$sqlQuery .= "AND Genre != 'ADULT ENTERTAINMENT' AND Genre != 'ADULT-HARDCORE' AND Genre != 'ADULT' AND Genre != 'CULT-EROTICA' AND Genre != 'ANIME-EROTIC' AND Genre2 != 'ADULT ENTERTAINMENT' AND Genre2 != 'ADULT-HARDCORE'";
+
+//Concatenate display order
+if($usersOrderBy != "Best Match")
+{
+	if($usersOrderBy == "Artist A-Z")
+	{
+		$sqlQuery .= " ORDER BY Artist, Title";
+	}
+	else if($usersOrderBy == "Title A-Z")
+	{
+		$sqlQuery .= " ORDER BY Title, Artist";
+	}
+	else if($usersOrderBy == "Release Date (Newest First)")
+	{
+		$sqlQuery .= " ORDER BY STR_TO_DATE(StreetDate,'%m/%d/%Y') DESC, Artist, Title";
+	}
+	else if($usersOrderBy == "Release Date (Oldest First)")
+	{
+		$sqlQuery .= " ORDER BY STR_TO_DATE(StreetDate,'%m/%d/%Y') ASC, Artist, Title";
+	}
+	else if($usersOrderBy == "Price (Ascending)")
+	{
+		$sqlQuery .= " ORDER BY MSLP ASC, Artist, Title";
+	}
+	else if($usersOrderBy == "Price (Descending)")
+	{
+		$sqlQuery .= " ORDER BY MSLP DESC, Artist, Title";
+	}
+}
+else if($usersSearchType == "Loose Match")
+{
+	$sqlQuery .= " ORDER BY relevance DESC";
+}
+
+//echo "<br/>SQL Query: " . $sqlQuery . "<br/><br/>";
+
+//Run the query the user typed in the form
+$resultFromQuery = mysql_query($sqlQuery);
+
+if($resultFromQuery)
+{
+	//Get the number of tuples in the result
+	$numberOfTuples = mysql_num_rows($resultFromQuery);
+
+	if (!$numberOfTuples) 
+	{
+//    die('Error - Query failed: ' . mysql_error());
+    die('<br/>No results found.<br/>');
+	}
+	
+	echo "<span style=\"color:#F00; font-size:20px; font-style:italic;\">Item availability is based on current stock at distributor. Please call Finders Records at (419) 352-7677 to check in-store availability.<br/><br/>MSLP is provided as a rough estimate of price and may not correspond to in-store price.</span><br/><br/>";
+	echo "<p>Found " . $numberOfTuples . " results:</p>";
+
+	//print the tuples as a nice table
+	print "<table width=\"650px\">";
+	
+	
+	// id, LblStdNm, CatalogNum, ConfigAbbr, Artist, Title, UPC, MSLP, QtyAvail, StreetDate, CntryOrgn, Genre, Genre2
+	
+	for($index=0; $index<$numberOfTuples; $index++) {
+		print "<th align=\"left\" colspan=\"2\">" . ($index+1) . "</th>";
+		
+			//get the next tuple
+			$row = mysql_fetch_array($resultFromQuery);
+	
+			//Format the result cell
+			if(strlen($row[5]) > 34)
+				print "<tr><td><span style=\"color:#fff350; font-size:22px\">" . substr($row[5], 0, 31) . "...</span><br/>";
+			else
+				print "<tr><td><span style=\"color:#fff350; font-size:22px\">" . $row[5] . "</span><br/>";
+			if(strlen($row[4]) > 40)
+				print "<span style=\"font-size:20px\">" . substr($row[4], 0, 37) . "...</span><br/>";
+			else
+				print "<span style=\"font-size:20px\">" . $row[4] . "</span><br/>";
+			if($row[3] == "ACC")
+				print "<span style=\"font-size:16px\">Accessory</span><br/>";
+			else
+				print "<span style=\"font-size:16px\">" . $row[3] . "</span><br/>";
+			print "<span style=\"font-size:14px\">[ " . $row[2] . " ]</span><br/><br/>";
+			print "<span style=\"font-size:16px\">Genre: " . $row[11] . " - " . $row[12] . "</span><br/>";
+			print "<span style=\"font-size:16px\">MSLP: \$" . $row[7] . "</span><br/></td>";
+			
+			print "<td style=\"padding-right:30px; font-size:16px;\">Release Date: " . $row[9] . "<br/>";
+			if($row[10] != "")
+				print "Country: " . $row[10] . "<br/><br/>";
+			else
+				print "<br/><br/>";
+			print "<span style=\"font-size:16px\">Label: " . $row[1] . "<br/>";
+			print "<span style=\"font-size:16px\">UPC: " . $row[6] . "<br/><br/>";
+			if($row[8] > 0)	{print "Available at Distributor";}
+			else			{print "On Backorder";}
+			print "</td></tr><tr><td>&nbsp;</td></tr>";
+	}
+	
+	print "</table></div>";
+}
+else
+{
+	echo "\nNo results found.<br/>";
+}
+
+?>
+
+          
+      </div>
+    
+	</section>
+	<!-- End Right Column -->
+
+    <!-- Begin Footer -->
+	<footer>
+    	<!-- Begin Full Site Link (Mobile Only) -->
+        <div class="fullSiteLink">
+          <p><a href="full.html" target="_self">Full Site</a></p>
+        </div>
+        <!-- End Full Site Link (Mobile Only) -->
+        
+        <!-- Being Creative Commons License -->
+    	<p class="ccLicense"><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/80x15.png" /></a><br /><small><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">FindersRecords.com</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License</a>.</small></p>
+        <!-- End Creative Common License -->
+        
+	</footer>
+	<!-- End Footer -->
+    
+    </div>
+        <!-- End Page Container -->
+
+        <script src="ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+        <script src="js/parsley.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics -->
+        <script>
+            _gaq.push(['_setAccount', 'UA-34048592-1']);
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
