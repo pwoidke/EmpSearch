533d8783c6e692b65de677423d17b4aeeda55b0b
Paul W. <pwoidke@gmail.com>
2013-06-18 14:00:27 -0400

    Unified CSS, Swiched lightbox, Added modal window
    Unified CSS across all pages. Swiched lightbox from Slimbox to Fresco
    because I was tired of it breaking for no reason. Added modal window
    support (commented out code on home page with example). Switched RSD
    Countdown back to Flash because I found some issues with it. Upgraded to
    jQuery 1.10.0. Go Falcons.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-$('.modalOpen').click(function(){
-    $('.modalDialog')
-        .css({display:'block'})
-        .animate({opacity:"1.0"}, 300);
-});
+$(document).ready(function(){
 
-$('.modalDialog, .modalClose').click(function(){
-    $('.modalDialog')
-        .animate({opacity:"0.0"}, 300, function(){
-            $('.modalDialog').css({display:'none'});
-        })
-});
-
-$('.modalContent').click(function(e){
-    e.stopPropagation();
+    $('.modalLink').modal({
+        trigger: '.modalLink',          // id or class of link or button to trigger modal
+        olay:'div.overlay',             // id or class of overlay
+        modals:'div.modal',             // id or class of modal
+        animationEffect: 'fadeIn',      // overlay effect | slideDown or fadeIn | default=fadeIn
+        animationSpeed: 400,            // speed of overlay in milliseconds | default=400
+        moveModalSpeed: 'fast',         // speed of modal movement when window is resized | slow or fast | default=false
+        background: 'aaaaaa',           // hexidecimal color code - DONT USE #
+        opacity: 0.7,                   // opacity of modal |  0 - 1 | default = 0.8
+        openOnLoad: false,              // open modal on page load | true or false | default=false
+        docClose: true,                 // click document to close | true or false | default=true    
+        closeByEscape: true,            // close modal by escape key | true or false | default=true
+        moveOnScroll: true,             // move modal when window is scrolled | true or false | default=false
+        resizeWindow: true,             // move modal when window is resized | true or false | default=false
+        video: 'http://player.vimeo.com/video/2355334?color=eb5a3d',    // enter the url of the video
+        videoClass:'video',             // class of video element(s)
+        close:'.closeBtn'               // id or class of close button
+    });
 });
